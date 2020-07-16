bb2a41c2d7f897f82339a6307a99f79c68938064
Baruch Phillips <baruchphillips@gmail.com>
2020-07-16 13:49:31 -0400

    first commit again

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ const db = require('./config/.env').mongoURI
 mongoose.connect(db)
     .then(() => console.log("MongoDB connected"))
     .catch(() => console.log(err))
+
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => console.log(`Server started on port ${port}`))
